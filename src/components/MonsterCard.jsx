ddd097dbaec6f72b29797b201b93925b594b04c4
Juan David Diaz <jdda886@gmail.com>
2024-06-03 18:17:57 -0400

    edit button implemented, must be tested properly

--- src/components/MonsterCard.jsx
+++ src/components/MonsterCard.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import EditLastSeen from "../pages/EditLastSeen";
 
 
 const API_URL = "https://cats-dogs-123123.adaptable.app/monsters/";
@@ -45,6 +46,7 @@ function MonsterCard() {
           </div>
         </div>
       )}{" "}
+      <EditLastSeen/>
     </div>
   );
 }
--- src/pages/EditLastSeen.jsx
+++ src/pages/EditLastSeen.jsx
@@ -2,16 +2,36 @@ import React, { useEffect, useState } from "react";
 const API_URL = "https://cats-dogs-123123.adaptable.app/monsters/";
 
 function EditLastSeen() {
-  const [updateLastSeen, setUpdateLastSeen] = useState(
-    monsters.find((monster) => monster.id === monsterId)
-  );
+  const [updateLastSeen, setUpdateLastSeen] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleUpdate = async (e, monsterId) => {
     e.preventDefault();
+    console.log("handle update here", e);
+    try {
+      const response = await axios.put(`API_URL/${monsterId}`, {
+        lastSeen: updateLastSeen,
+      });
+      console.log("respose updated", response.data);
+    } catch (error) {
+      console.log("here is an error on update", error);
+    }
+  };
 
-  }
-
-  return <div></div>;
+  return (
+    <div>
+      <form onSubmit={handleUpdate}>
+        <label htmlFor="lastSeen">Update Last Seen:</label>
+        <input
+          type="text"
+          id="lastSeen"
+          value={updateLastSeen}
+          onChange={(e) => setUpdateLastSeen(e.target.value)}
+          required
+        />
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
 }
 
 export default EditLastSeen;
